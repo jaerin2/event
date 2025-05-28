d28ff23769c789853be54f142695f6b918f95b27
jaerin2 <129550769+jaerin2@users.noreply.github.com>
2025-05-28 03:43:54 +0000

    쿠폰 리디렉션 절대 URL로 수정

--- api/redirect.js
+++ api/redirect.js
@@ -2,19 +2,19 @@ export default function handler(req, res) {
   const { serial } = req.query;
 
   const couponRedirects = {
-    "501840123": "/VFREA523BAR01QP5",
-    "306236584": "/OAWBQ195BAO42YQ1",
-    "205356356": "/PVAQN014NAQ53WI0",
-    "102533233": "/BAOVA502QFP14BA1",
-    "104589325": "/QRPWV325ATB30QA2",
-    "103025932": "/ASBFH925BOB62QP3",
-    "104012421": "/BOAAP104NHK39RW4",
-    "102180421": "/JBVOA402BQP10LZ5",
-    "104218214": "/KAFAQ620YUO35CN6",
-    "109523889": "/TRWQC052ALS50IR7",
-    "105298312": "/VOBAQ520BWL60RI8",
-    "103250815": "/DEQSL402XVB29QL9",
-    "100924523": "/AOVDR602BMW49WL0"
+    "501840123": "https://granviastar.co.kr/VFREA523BAR01QP5",
+    "306236584": "https://granviastar.co.kr/OAWBQ195BAO42YQ1",
+    "205356356": "https://granviastar.co.kr/PVAQN014NAQ53WI0",
+    "102533233": "https://granviastar.co.kr/BAOVA502QFP14BA1",
+    "104589325": "https://granviastar.co.kr/QRPWV325ATB30QA2",
+    "103025932": "https://granviastar.co.kr/ASBFH925BOB62QP3",
+    "104012421": "https://granviastar.co.kr/BOAAP104NHK39RW4",
+    "102180421": "https://granviastar.co.kr/JBVOA402BQP10LZ5",
+    "104218214": "https://granviastar.co.kr/KAFAQ620YUO35CN6",
+    "109523889": "https://granviastar.co.kr/TRWQC052ALS50IR7",
+    "105298312": "https://granviastar.co.kr/VOBAQ520BWL60RI8",
+    "103250815": "https://granviastar.co.kr/DEQSL402XVB29QL9",
+    "100924523": "https://granviastar.co.kr/AOVDR602BMW49WL0"
   };
 
   if (serial && couponRedirects[serial]) {
